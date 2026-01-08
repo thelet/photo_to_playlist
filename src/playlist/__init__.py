4f74741fa56417fb85cfb02422e27db79fa083cc
thelet <thelet.shevach@gmail.com>
2026-01-08 15:12:13 +0200

    Fix: update playlist __init__ to export get_spotify_credentials

--- src/playlist/__init__.py
+++ src/playlist/__init__.py
@@ -4,13 +4,13 @@ Generates playlists from song parameters using Deezer and Spotify APIs
 """
 
 from .deezer import DeezerPlaylistGenerator
-from .spotify_client import SpotifyClient, load_credentials_from_file
+from .spotify_client import SpotifyClient, get_spotify_credentials
 from .oauth_server import OAuthCallbackServer, start_callback_server
 
 __all__ = [
     "DeezerPlaylistGenerator",
     "SpotifyClient",
-    "load_credentials_from_file",
+    "get_spotify_credentials",
     "OAuthCallbackServer",
     "start_callback_server",
 ]
